34e93a1fe1e3d6a2a1e5987e9119a989b35653ee
sonigay <43840048+sonigay@users.noreply.github.com>
2020-01-06 01:58:38 +0900

    Update test_init_final.py

--- test_init_final.py
+++ test_init_final.py
@@ -984,22 +984,21 @@ while True:
 							await msg.channel.send(embed=embed, tts=False)
 		else :
 			message = await client.get_channel(channel).fetch_message(msg.id)
-
-						if message.content.startswith(command[22]):
-							if basicSetting[10] !="" and basicSetting[12] !="" and basicSetting[17] !="" and basicSetting[18] !="" and basicSetting[19] !=""  :
-								SearchID = message.content[len(command[22])+1:]
-								gc = gspread.authorize(credentials)
-								wks = gc.open(basicSetting[12]).worksheet(basicSetting[17])
+			if message.content.startswith(command[22]):
+				if basicSetting[10] !="" and basicSetting[12] !="" and basicSetting[17] !="" and basicSetting[18] !="" and basicSetting[19] !=""  :
+					SearchID = message.content[len(command[22])+1:]
+					gc = gspread.authorize(credentials)
+					wks = gc.open(basicSetting[12]).worksheet(basicSetting[17])
 							
-								wks.update_acell(basicSetting[18], SearchID)
+					wks.update_acell(basicSetting[18], SearchID)
 							
-								result = wks.acell(basicSetting[18]).value
+					result = wks.acell(basicSetting[18]).value
 							
-								embed = discord.Embed(
-										description= '```' + SearchID + ' ' + result + ' ```',
-										color=0xff00ff
-										)
-								await msg.channel.send(embed=embed, tts=False)
+					embed = discord.Embed(
+							description= '```' + SearchID + ' ' + result + ' ```',
+							color=0xff00ff
+									)
+					await msg.channel.send(embed=embed, tts=False)
 			else :
 				message = await client.get_channel(channel).fetch_message(msg.id)
 
